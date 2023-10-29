cf2b56558d92af60bd3cf53bf6fb197ed9885a7b
Herson Guerrero Huh <83830469+herson-xyz@users.noreply.github.com>
2023-10-28 21:14:32 -0400

    Color space conversion - two approaches, linear to sRGB + pow conversion

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -15,6 +15,16 @@ float remap(float v, float inMin, float inMax, float outMin, float outMax) {
   return mix(outMin, outMax, t);
 }
 
+// SimonDev, GLSL Shaders From Scratch, https://simondev.teachable.com/courses/1783153
+vec3 linearTosRGB(vec3 value ) {
+  vec3 lt = vec3(lessThanEqual(value.rgb, vec3(0.0031308)));
+  
+  vec3 v1 = value * 12.92;
+  vec3 v2 = pow(value.xyz, vec3(0.41666)) * 1.055 - vec3(0.055);
+
+	return mix(v2, v1, lt);
+}
+
 void main() {
     vec4 textureColor  = texture2D(uTexture, fract(vUv * 10.0));
 
@@ -27,13 +37,7 @@ void main() {
     float hemiMix      = remap(normal.y, -1.0, 1.0, 0.0, 1.0);  
     vec3  hemiLight    = mix(groundLight, skyLight, hemiMix);   
 
-    vec3  ambientLight = vec3(0.5);
-
-    /* 
-        Lambertian Lighting Model
-        - Figure out the direction of the light source to this pixel
-        - Use the dot product between the normal and the light direcion to figure out how much light is hitting this pixel
-    */                             
+    vec3  ambientLight = vec3(0.5);                       
 
     vec3  lightDirection = normalize(vec3(1.0, 1.0, 1.0));
     float dp             = max(dot(normal, lightDirection), 0.0);
@@ -43,6 +47,15 @@ void main() {
     lighting += ambientLight * 0.0 + hemiLight * 0.5 + sunlight * 0.5;                 
 
     vec3  color          = textureColor.rgb + lighting;
+
+    /*  
+        - We need to convert the final color into sRGB, 
+          with either the linear to sRGB conversion function 
+          or the pow(1.0 / 2.2) approximation.
+    */      
+
+    //color                = linearTosRGB(color);       // linear to sRGB conversion
+    color                = pow(color, vec3(1.0 / 2.2)); // pow(1.0 / 2.2) approximation
 
     gl_FragColor         = vec4(color, 1.0);
 }

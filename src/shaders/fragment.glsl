e25b6945295e75574088e47c2d83a08d56ef8a30
Herson Guerrero Huh <83830469+herson-xyz@users.noreply.github.com>
2023-10-28 23:34:05 -0400

    Use step on specular and fresnel to apply cell shading effect

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -35,7 +35,7 @@ void main() {
 
     vec3 viewDirection = normalize(cameraPosition - vPosition);
 
-    vec3  ambientLight = vec3(0.5);                       
+    vec3  ambientLight = vec3(1.0);                       
 
     vec3  skyLight     = vec3(0.0, 0.3, 0.6);                   
     vec3  groundLight  = vec3(0.3, 0.6, 0.1);                   
@@ -44,32 +44,43 @@ void main() {
 
     vec3  lightDirection = normalize(vec3(1.0, 1.0, 1.0));
     float dp             = max(0.0, dot(normal, lightDirection));
+
+    /*
+        Cell Shading
+        - Build "steppiness" into the lighting
+    */
+
+    dp *= smoothstep(0.5, 0.505, dp);
+
     vec3  sunlightColor  = vec3(1.0, 1.0, 0.9);
     vec3  sunlight       = sunlightColor * dp;
+    
 
-    // Phong Specular
+    /*
+        Specular
+        - We use smoothstep to make the specular highlight more pronounced
+        - As is, it still has a gradual falloff, but we can make it more pronounced by
+          increasing the value of the second parameter
+    */
+    vec3 specular = vec3(0.0);
     vec3 r = normalize(reflect(-lightDirection, normal));
     float phongValue = max(dot(r, viewDirection), 0.0);
     phongValue = pow(phongValue, 32.0);
 
-    vec3 specular = vec3(phongValue);
 
-    vec3 iblCoord = normalize(reflect(-viewDirection, normal));
-    vec3 iblSample = textureCube(specMap, iblCoord).rgb;
-
-    specular += iblSample * 0.5;
-
-    /*  
-        - Take the dot product of the view direction and the normal
+    /*
+        Fresnel
+        - Introduce rim lighting
     */
 
-    // Fresnel
     float fresnel = 1.0 - max(0.0, dot(viewDirection, normal));
     fresnel = pow(fresnel, 2.0);
+    fresnel *= step(0.5, fresnel);
 
-    specular *= fresnel;
-    
-    lighting += ambientLight * 0.0 + hemiLight * 0.5 + sunlight * 0.5;                 
+    specular += phongValue;
+    specular = smoothstep(0.5, 0.51, specular);          
+
+    lighting += hemiLight * (fresnel + 0.5) + sunlight * 0.5;       
 
     vec3  color          = textureColor.rgb * 0.2 + lighting + specular;   
 
